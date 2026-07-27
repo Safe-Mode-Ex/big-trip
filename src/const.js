48eeb37eb263e8e809f21795a41b255699634f0f
Sergei Dmitriev <Sergey.Dmitriev@mvideo.ru>
2026-07-28 00:25:58 +0300

    Шаблонизирует фильтры

--- src/const.js
+++ src/const.js
@@ -1,4 +1,10 @@
 const TYPES = ['Taxi', 'Bus', 'Train', 'Ship', 'Drive', 'Flight', 'Check-in', 'Sightseeing', 'Restaurant'];
+const FilterType = {
+  ALL: 'everything',
+  FUTURE: 'future',
+  PRESENT: 'present',
+  PAST: 'past',
+};
 const MS_IN_SEC = 1000;
 const SEC_IN_MIN = 60;
 const MIN_IN_HOUR = 60;
@@ -8,4 +14,4 @@ const MS_IN_DAY = MS_IN_HOUR * HOUR_IN_DAY;
 const DEC_RADIX = 10;
 const KEY_ESCAPE = 'Escape';
 
-export {TYPES, MS_IN_HOUR, MS_IN_DAY, DEC_RADIX, KEY_ESCAPE};
+export {TYPES, FilterType, MS_IN_HOUR, MS_IN_DAY, DEC_RADIX, KEY_ESCAPE};
