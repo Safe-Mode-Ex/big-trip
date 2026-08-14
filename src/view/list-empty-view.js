d695f7704909508b71ba094bd544309fe77b2dfe
Sergei Dmitriev <Sergey.Dmitriev@mvideo.ru>
2026-08-14 18:23:42 +0300

    Исправит функции фильтрации

--- src/view/list-empty-view.js
+++ src/view/list-empty-view.js
@@ -4,8 +4,8 @@ import { FilterType } from '../const';
 const ListEmptyTextType = {
   [FilterType.ALL]: 'Click New Event to create your first point',
   [FilterType.FUTURE]: 'There are no future events now',
+  [FilterType.PRESENT]: 'There are no present events now',
   [FilterType.PAST]: 'There are no past events now',
-  [FilterType.FUTURE]: 'There are no future events now',
 };
 
 function createListEmptyTemplate(filterType) {
